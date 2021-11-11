640d598d4300715f6d05c5fa88152f193407b857
OTree <kevin10410@gmail.com>
2021-11-11 22:04:55 +0800

    feat(default): implement google font to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-    <Header/>
     <nuxt/>
   </div>
 </template>
 <script>
-import Header from '~/components/Header.vue';
-
-export default {
-  components: {
-    Header,
-  },
-};
 </script>
+<style>
+html {
+  font-family: 'Open Sans', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
