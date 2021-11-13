cb8c99d0283d3593b30c883ce0820cf445d5dc7c
OTree <kevin10410@gmail.com>
2021-11-13 15:07:08 +0800

    feat(default layout ): implement header & side nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,28 @@
 <template>
   <div>
+    <TheHeader @sidenavToggle="showSideNav = !showSideNav"/>
+    <TheSidenav
+      :show="showSideNav"
+      @close="showSideNav = false"
+    />
     <nuxt/>
   </div>
 </template>
 <script>
+import TheHeader from "~/components/Navigation/TheHeader.vue";
+import TheSidenav from "~/components/Navigation/TheSidenav.vue";
+
+export default {
+  components: {
+    TheHeader,
+    TheSidenav,
+  },
+  data() {
+    return {
+      showSideNav: false,
+    };
+  },
+};
 </script>
 <style>
 html {
