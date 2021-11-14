99c8ccf8854f8ec789cbd260e01cb810eff56466
OTree <kevin10410@gmail.com>
2021-11-14 21:28:24 +0800

    feat(error): implement error layout

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>404 Page Not Fount.</h1>
+  <div class="error-page">
+    <h1>404 Page Not Fount.</h1>
+    <p>Back to <a href="/">Home Page</a></p>
+  </div>
 </template>
+<style scoped>
+.error-page {
+  text-align: center;
+}
+
+.error-page a {
+  color: red;
+  text-decoration: none;
+}
+</style>
